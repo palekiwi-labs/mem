ebe70e4954c1010fd8f58e58c30aae836307598f
Pawel Lisewski <dev@palekiwi.com>
2026-04-12 11:40:27 +0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "mem: a file-based memory system for agentic workflows";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, fenix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        rustToolchain = fenix.packages.${system}.stable.toolchain;
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "mem";
+          version = "0.1.0";
+          src = pkgs.lib.cleanSource ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = [ rustToolchain ];
+
+          buildInputs = [];
+
+          meta = with pkgs.lib; {
+            description = "A new Rust project";
+            license = licenses.mit;
+            maintainers = [ ];
+          };
+        };
+
+        devShells.default = pkgs.mkShell
+          {
+            buildInputs = [
+              rustToolchain
+              pkgs.rust-analyzer
+              pkgs.cargo-expand
+              pkgs.cargo-watch
+              pkgs.cargo-edit
+              pkgs.cargo-nextest
+            ];
+
+            shellHook = ''
+              echo "Rust development environment ready!"
+              echo "Rust version: $(rustc --version)"
+            '';
+          };
+      });
+}
